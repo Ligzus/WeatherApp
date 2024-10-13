a60a40511e1f5956c8210c570c546d8f2bc4a26a
Ligzus2 <infopop@mail.ru>
2024-10-13 15:25:30 +0300

    отредактировал стили

--- weather-app/src/components/WeatherCard/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard/WeatherCard.jsx
@@ -5,13 +5,19 @@ const WeatherCard = ({ weather }) => {
   return (
     <div className={styles.card}>
       <div className={styles.city}>
-        <h2>{weather.city.toUpperCase()}</h2>
-        <img
-          src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
-          alt="weather icon"
-        />
+        <div>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
+            alt="weather icon"
+          />
+          <h2>{weather.city.toUpperCase()}</h2>
+        </div>   
+        <div className={styles.weather}>
+          <p className={styles.temp}>{Math.round(weather.temp)}</p>
+          <span>°C</span>
+        </div>      
       </div>
-      <p>Температура: {Math.round(weather.temp)}°C</p>
+
       <p>Скорость ветра: {weather.windSpeed} м/с</p>
       <p>Влажность: {weather.humidity}%</p>
     </div>
